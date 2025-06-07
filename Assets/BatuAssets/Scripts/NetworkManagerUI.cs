e9cca44c77dd37b94e404017b76c90de70ae16a2
Batuhan Şengül <80199404+bathuchan@users.noreply.github.com>
2025-06-08 01:07:17 +0300

    server build

--- Assets/BatuAssets/Scripts/NetworkManagerUI.cs
+++ Assets/BatuAssets/Scripts/NetworkManagerUI.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
@@ -16,10 +15,21 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] private string ipAdress="127.0.0.1";
     [SerializeField] private string portAdress="7777";
 
+    [SerializeField] private BuildType currentBuildType;
+    [SerializeField] private GameObject uiButtonsParent;
+   enum BuildType 
+    {
+        Server,
+        Host, 
+        Client,
+        Null
+    }
+
+    UnityTransport unityTransport;
     private void Awake()
     {
         //UnityTransport unityTransport = NetworkManager.Singleton.NetworkConfig.NetworkTransport as UnityTransport;
-        UnityTransport unityTransport = networkManager.NetworkConfig.NetworkTransport as UnityTransport;
+         unityTransport = networkManager.NetworkConfig.NetworkTransport as UnityTransport;
 
         serverBtn.onClick.AddListener(() =>
         {
@@ -38,6 +48,42 @@ public class NetworkManagerUI : MonoBehaviour
             ApplyConnectionSettings(unityTransport);
             NetworkManager.Singleton.StartClient();
         });
+    }
+
+    private void Start()
+    {
+        switch (currentBuildType)
+        {
+            case BuildType.Client:
+                // code block
+                ApplyConnectionSettings(unityTransport);
+                NetworkManager.Singleton.StartClient();
+                uiButtonsParent.SetActive(false);
+
+
+
+                break;
+            case BuildType.Server:
+                // code block
+                ApplyConnectionSettings(unityTransport);
+                NetworkManager.Singleton.StartServer();
+                uiButtonsParent.SetActive(false);
+
+                break;
+
+            case BuildType.Host:
+                ApplyConnectionSettings(unityTransport);
+                NetworkManager.Singleton.StartHost();
+                uiButtonsParent.SetActive(false);
+
+                break;
+            default:
+                Debug.LogWarning("Build type not declared!");
+                uiButtonsParent.SetActive(true);
+
+                break;
+        }
+
     }
 
     private void ApplyConnectionSettings(UnityTransport transport)
